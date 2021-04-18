090520d1bdc35e794ae860cfbd2c985f71909081
imane-benchaiba <imyben.bi@gmail.com>
2021-04-19 00:27:57 +0200

    final result

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Image from './moi.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  Constructor(props){
+    //super(props);
+    this.state = {
+      person : {
+        fullName:"Imane BENCHAIBA",
+        bio:"", 
+        imgSrc:Image, 
+        profession:"Rédactrice web"
+      },
+      shows : false
+    }
+  }
+    
+  handleClick=()=>this.setState({shows:true});
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}> Shows </button>
+        {shows ? 
+          <div>
+            <p> {person.fullName} </p> 
+            <p> {person.bio} </p> 
+            <p> {person.imgSrc} </p> 
+            <p> {person.profession} </p> 
+          </div>
+        : {}
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
